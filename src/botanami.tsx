0c9493ae53616678ef84f0d26cf93886ee1ac5c7
Sean Anastasi <sean@anasta.si>
2023-09-08 22:59:07 -0700

    Try to get some error message out of failing load

--- src/botanami.tsx
+++ src/botanami.tsx
@@ -2,7 +2,16 @@ import { App } from "./app";
 import { createRoot } from "react-dom/client";
 import React from "react";
 
+throw new Error("died");
+
 void (async function() {
+    function loadStatus(message: string) {
+        const element = document.getElementById("load-status");
+        if (element) {
+            element.innerText = message;
+        }
+    }
+
     try {
         const registration = await navigator.serviceWorker.register(
             new URL("service-worker.ts", import.meta.url), {
@@ -13,7 +22,8 @@ void (async function() {
         if (installingOrWaiting) {
             await new Promise<void>((resolve, reject) => {
                 installingOrWaiting.onstatechange = function() {
-                    console.log("state:", installingOrWaiting.state)
+                    console.log("state:", installingOrWaiting.state);
+                    loadStatus(`state: ${installingOrWaiting.state}`);
                     if (installingOrWaiting.state === 'activated') {
                         console.log('Service worker installed. Resolving');
                         resolve();
@@ -32,5 +42,5 @@ void (async function() {
     const container = document.createElement("div");
     const root = createRoot(container)
     root.render(<App initialUrl={location.hash.replace(/^#/, "")} />);
-    document.body.appendChild(container);
+    document.body.childNodes[0].replaceWith(container);
 })();
